8a18d7389ab01053ffae9ec0b5be6eeb43d4e70a
Valentina Escobar <41376511+valentinaescobar31@users.noreply.github.com>
2018-08-25 18:14:47 -0500

    Update agencia.h

--- agencia.h
+++ agencia.h
@@ -1,43 +1,46 @@
-#ifndef RUTA_H_INCLUDED
-#define RUTA_H_INCLUDED
-#include <fstream>
+#ifndef AGENCIA_H_INCLUDED
+#define AGENCIA_H_INCLUDED
+#include <iostream>
+#include <list>
+
 using namespace std;
 
-class ruta
+class agencia
 {
+
 protected:
 
-    string codigoVuelo;
-    string dia;
-    string ciudadO;
-    string ciudadD;
-    unsigned int horaSalida;
-    unsigned int tiempoVuelo;
-    unsigned int cantSillas;
-    double costoSilla;
+    string idAgencia;
+    string contrasena;
+    string nombre;
+    list <ruta> rutas;
+    list <venta> ventas;
 
 public:
 
-    ruta();
-    virtual ~ruta();
-    void SetCodigoVuelo(string cod);
-    void SetDia (string d);
-    void SetCiudadO(string origen);
-    void SetCiudadD(string destino);
-    void SetHoraSalida (unsigned int horaS);
-    void SetTiempoVuelo (unsigned int temp);
-    void SetCostoSilla (double costo);
+    agencia();
+    virtual ~agencia();
 
-    string GetCodigoVuelo();
-    string GetDia();
-    string GetCiudadO();
-    string GetCiudadD();
-    unsigned int GetHoraSalida();
-    unsigned int GetTiempoVuelo();
-    double GetCostoSilla();
+    void SetIdAgencia(string idA);
+    void SetContrasena (string cont);
+    void SetNombre (string nom);
+    void SetRutas (list <ruta> rut);
+    void SetVentas (list <venta> ven);
+
+    string GetIdAgencia();
+    string GetContrasena();
+    string GetNombre();
+    list <venta> GetVentas();
+    list <ruta> GetRutas();
+
+
+
+    string verificarAgencia(string nomArchivo);
+
+
+
 
 
 };
 
-
-#endif // RUTA_H_INCLUDED
+#endif // AGENCIA_H_INCLUDED
