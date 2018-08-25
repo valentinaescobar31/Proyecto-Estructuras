ab827651d167ffef58d1d7f9f975c32bd402a605
Valentina Escobar <41376511+valentinaescobar31@users.noreply.github.com>
2018-08-25 18:15:16 -0500

    Update central.h

--- central.h
+++ central.h
@@ -1,6 +1,21 @@
 #ifndef CENTRAL_H_INCLUDED
 #define CENTRAL_H_INCLUDED
 
+using namespace std;
+class central
+{
 
+protected:
+
+
+
+public:
+
+    void CargarArchivoRuta (string nomArchivo);
+    void CargarArchivoVenta (string nomArchivo);
+    void CargarArchivoContrasena (string nomArchivo);
+
+
+};
 
 #endif // CENTRAL_H_INCLUDED
